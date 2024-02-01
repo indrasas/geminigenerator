bb875fc9ab0604ab50b7e31ed79bc8845ad3c8eb
Insrasas <137709253+indrasas@users.noreply.github.com>
2024-02-01 17:07:54 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -9,8 +9,22 @@ genai.configure(api_key=api_key)
 generation_config = {"temperature":0.9,"top_p":1,"top_k":1,"max_output_tokens":2048}
 model = genai.GenerativeModel("gemini-pro",generation_config=generation_config)
 
-st.title("Gemini AI Article Generator")
+adsterra_code = """
+<script type="text/javascript">
+    atOptions = {
+        'key': '2e1fc6b4812b3af1e887c425bd9febd8',
+        'format': 'iframe',
+        'height': 250,
+        'width': 300,
+        'params': {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="//demiseskill.com/2e1fc6b4812b3af1e887c425bd9febd8/invoke.js"></scr' + 'ipt>');
+</script>
+"""
 
+
+st.title("Gemini AI Article Generator")
+st.markdown(adsterra_code, unsafe_allow_html=True)
 keyword = st.text_input("Enter a keyword")
 language = st.text_input("Enter language")
 writing_style = st.selectbox("Select writing style:", ["Casual", "Informative", "Witty"])
