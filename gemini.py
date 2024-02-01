74e333d205dfad477958c9485737f3b7fead1f0c
Insrasas <137709253+indrasas@users.noreply.github.com>
2024-02-01 18:15:23 +0800

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -24,24 +24,27 @@ genai.configure(api_key=api_key)
 generation_config = {"temperature": 0.9, "top_p": 1, "top_k": 1, "max_output_tokens": 2048}
 model = genai.GenerativeModel("gemini-pro", generation_config=generation_config)
 
+st.set_page_config(
+   page_title="Article Generator",
+   page_icon="🧊",
+)
+
+
 st.title("Article Generator")
+components.html("""
+<script type='text/javascript' src='//demiseskill.com/bf/48/25/bf48250f632348ae4ae0dd43a3a7b1b8.js'></script>""")
 keyword = st.text_input("Enter a keyword")
 language = st.text_input("Enter language")
 st.caption('Insert language for the result. ex : Indonesia, English, Japanese, French, Korean, Germany, Chinese Simplified')
 writing_style = st.selectbox("Select writing style:", ["Casual", "Informative", "Witty"])
 word_count = st.slider("Select word count:", min_value=300, max_value=1000, step=100, value=300)
-components.html("""
-<script type='text/javascript' src='//demiseskill.com/bf/48/25/bf48250f632348ae4ae0dd43a3a7b1b8.js'></script>""")
-
 if st.button("Generate Article"):
-
         prompt = f"Generate an SEO-optimized article about {keyword} in {language}. The article should be approximately {word_count} words long and structured with clear headings, subheadings, and paragraphs. Use relevant keywords throughout the text and create engaging content that effectively addresses the topic."
-
         # Call the Gemini API with the prompt, requesting only text
         response = model.generate_content(prompt)
-
         # Extract the generated text
         article_text = response.text
-
         # Display the generated text
         st.write(article_text)
+
+
